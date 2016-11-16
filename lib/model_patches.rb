0454a4038045e3cd6126dac58d3a4ce05e7a0048
lizconlan <liz@mysociety.org>
2016-11-16 10:50:20 +0000

    Add law_used overrides

--- lib/model_patches.rb
+++ lib/model_patches.rb
@@ -6,6 +6,28 @@
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
 Rails.configuration.to_prepare do
+  # Remove UK-specific references to FOI
+  InfoRequest.instance_eval do
+
+    THEME_LAW_USED_READABLE_DATA =
+      { :foi => { :short => 'accesso civico',
+                  :full => 'Decreto Trasparenza (D.lgs. 33/2013)',
+                  :with_a => _('A Freedom of Information request'),
+                  :act => 'D.lgs. 33/2013' }
+      }
+
+  end
+
+  InfoRequest.class_eval do
+    def applicable_law
+      begin
+        THEME_LAW_USED_READABLE_DATA.fetch(law_used.to_sym)
+      rescue KeyError
+        raise "Unknown law used '#{law_used}'"
+      end
+    end
+  end
+
   OutgoingMessage.class_eval do
     # Add intro paragraph to new request template
     def default_letter
