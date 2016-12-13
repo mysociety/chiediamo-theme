56c573d91e17845150a4a48119e66e1e5a78c6ce
lizconlan <liz@mysociety.org>
2016-12-13 14:29:40 +0000

    Adapt AskTheEU's frontpage blog caching code
    Based on code at commit e787a9bf

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -8,8 +8,60 @@
 Rails.configuration.to_prepare do
   # Front page needs some additional info
   GeneralController.class_eval do
+
     def frontpage
-      blog
+      blog_cache("latest_blog_posts")
+    end
+
+    private
+
+    def blog_cache(cache_key, expires=4.hours)
+      # set @blog_items to an empty array and return if there's no blog feed
+      if AlaveteliConfiguration::blog_feed.empty?
+        @blog_items = []
+        return
+      end
+      # nothing to do here, call the blog code and return
+      return get_blog_content unless AlaveteliConfiguration::cache_fragments
+
+      # read in the last timestamp
+      updated = read_fragment("#{cache_key}-lastupdated")
+
+      # construct the fragment key
+      @fragment_key = "#{cache_key}-#{updated}"
+
+      # if the fragment is unreadable or has reached expiry
+      # attempt to pull in the blog feed
+      if updated.nil? || !fragment_exist?(@fragment_key) ||
+         Time.now > (Time.parse(updated) + expires)
+        # keep a note of the old key
+        old_key = @fragment_key.dup
+
+        # prepare a new timestamp and fragment key
+        updated = create_timestamp
+        @fragment_key = "#{cache_key}-#{updated}"
+
+        logger.info "attempting to pull in the feed"
+        get_blog_content
+
+        if @blog_items.empty?
+          # attempt to write the previous fragment to our new cache
+          fragment = read_fragment(old_key)
+          if fragment
+            logger.warn "Writing old blog fragment (#{old_key}) to " \
+                        "current cache (#{@fragment_key}) due to feed " \
+                        "error or timeout."
+            write_fragment(@fragment_key, fragment)
+          end
+        end
+
+        # store the new timestamp
+        write_fragment("#{cache_key}-lastupdated", updated)
+      end
+    end
+
+    def create_timestamp(time=Time.now)
+      time.strftime('%Y%m%d-%H:%M:%S')
     end
   end
 end
